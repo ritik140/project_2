1f24a8b734d49eae3299e5902e6ca038c6904586
Ritik <ritik@123.com>
2024-10-04 12:28:46 +0530

    Error Handling

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import dotenv from "dotenv";
-// import express from "express";
+import app from './app.js'
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("Hello there!");
-// });
-
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server is Listen to the Port: ${process.env.PORT}`);
-// });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000);
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
