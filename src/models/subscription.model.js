ef639e4930f8a6c08fa52c45d879f6f35905b1ad
Ritik <ritik@123.com>
2024-10-08 22:42:05 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId, //The one who is subscribing 
+        ref:"User"
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId, // the youtuber
+        ref:"User"
+    },
+
+
+
+
+}, { timestamps: true });
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
