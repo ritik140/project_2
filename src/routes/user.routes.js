ff612245071e3b33b5e2d0a2ee5ddbd00d7d02f5
Ritik <ritik@123.com>
2024-10-14 10:29:41 +0530

    Refactor user.routes.js to add new user account management features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
   registerUser,
   loginUser,
   logoutUser,
-  refreshAccessToken
+  refreshAccessToken,
+  resetPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  fileUploadAvatar,
+  fileUploadCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -27,6 +34,19 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
-router.route('/refreshToken').post(refreshAccessToken)
+router.route("/refreshToken").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, resetPassword);
+router.route("/current-user").post(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+router
+  .route("/avatar-update")
+  .patch(verifyJwt, upload.single("/avatar"), fileUploadAvatar);
+router
+  .route("/cover-image-update")
+  .patch(verifyJwt, upload.single("/coverImage"), fileUploadCoverImage);
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
