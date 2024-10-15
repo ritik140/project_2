009b368f8fdc07d2f84f0cac329e470f41e58617
Ritik <ritik@123.com>
2024-10-15 16:19:47 +0530

    Add comment, like, and playlist models

---
+++ src/models/likes.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    tweet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+  },
+  { timestamps: true }
+);
+
+export const like = mongoose.model("like", likeSchema);
